3afc5449493a10bb0702cd7f5cdb59d3adace996
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2021-08-10 18:34:15 -0300

    adicionando model, repository e resource de categoria

---
+++ src/main/java/com/vini/money/api/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.vini.money.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vini.money.api.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+}
