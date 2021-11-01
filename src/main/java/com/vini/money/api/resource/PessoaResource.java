d059f9d85daf35ead7df75133023ad2659d1ed48
vinicius <vinicius.tecbcc@gmail.com>
2021-11-01 19:34:41 -0300

    adicionando pageable default

--- src/main/java/com/vini/money/api/resource/PessoaResource.java
+++ src/main/java/com/vini/money/api/resource/PessoaResource.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -42,13 +43,13 @@ public class PessoaResource {
 	
 	@GetMapping
 	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and #oauth2.hasScope('read')")
-	public Page<Pessoa> listar(PessoaFilter pessoaFilter, Pageable pageable){
+	public Page<Pessoa> listar(PessoaFilter pessoaFilter, @PageableDefault(page = 0,size = 1000) Pageable pageable){
 		return pessoaRepository.filtrar(pessoaFilter,pageable);
 	}
 	
 	@GetMapping(params = "resumo")
 	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and #oauth2.hasScope('read')")
-	public Page<ResumoPessoa> resumo(PessoaFilter pessoaFilter, Pageable pageable){
+	public Page<ResumoPessoa> resumo(PessoaFilter pessoaFilter,@PageableDefault(page = 0,size = 1000) Pageable pageable){
 		return pessoaRepository.resumo(pessoaFilter,pageable);
 	}
 	
