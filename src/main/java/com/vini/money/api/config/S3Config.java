db7905644e38aceda53299c202195f92feaf0737
vinicius <vinicius.tecbcc@gmail.com>
2021-11-20 15:27:32 -0300

    profiles para o s3

--- src/main/java/com/vini/money/api/config/S3Config.java
+++ src/main/java/com/vini/money/api/config/S3Config.java
@@ -3,6 +3,7 @@ package com.vini.money.api.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
@@ -22,8 +23,8 @@ public class S3Config {
 	
 	@Autowired
 	private MoneyApiProperty property;
-	
-	@Bean
+	 
+	@Bean @Profile("dev")
 	public AmazonS3 amazonS3() {
 		System.out.println(property.getS3().getAccessKeyId() + " | "+property.getS3().getSecretAccessKey() + " | " + property.getS3().getBucket());
 		AWSCredentials credentials = new BasicAWSCredentials(property.getS3().getAccessKeyId(), property.getS3().getSecretAccessKey());
@@ -31,6 +32,36 @@ public class S3Config {
 		AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
 				.withCredentials(new AWSStaticCredentialsProvider(credentials))
 				.withRegion(Regions.US_EAST_2)
+				.build();
+		
+		//se o bucket nao existir no aws, cria ele automaticamente.
+		if(!amazonS3.doesBucketExistV2(property.getS3().getBucket())) {
+			amazonS3.createBucket(new CreateBucketRequest(property.getS3().getBucket()));
+			
+			BucketLifecycleConfiguration.Rule regraExpiracao = 
+					new BucketLifecycleConfiguration.Rule()
+					.withId("Regra de expiracao de arquivos temporarios")
+					.withFilter(new LifecycleFilter(new LifecycleTagPredicate(new Tag("expirar", "true"))))
+					.withExpirationInDays(1)
+					.withStatus(BucketLifecycleConfiguration.ENABLED);
+			
+			BucketLifecycleConfiguration configuration = new BucketLifecycleConfiguration()
+					.withRules(regraExpiracao);
+			
+			amazonS3.setBucketLifecycleConfiguration(property.getS3().getBucket(), configuration);
+		}
+		
+		return amazonS3;
+	}
+	
+	@Bean @Profile("prod")
+	public AmazonS3 amazonS3prod() {
+		System.out.println(property.getS3().getAccessKeyId() + " | "+property.getS3().getSecretAccessKey() + " | " + property.getS3().getBucket());
+		AWSCredentials credentials = new BasicAWSCredentials(property.getS3().getAccessKeyId(), property.getS3().getSecretAccessKey());
+		
+		AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
+				.withCredentials(new AWSStaticCredentialsProvider(credentials))
+				.withRegion(Regions.US_EAST_1)
 				.build();
 		
 		//se o bucket nao existir no aws, cria ele automaticamente.
